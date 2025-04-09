87c4ab1b51ed2f38362de2611d98c605e9d6cce7
Mukhammadaziz Sodikov <155058732+IamSodikov@users.noreply.github.com>
2025-04-09 11:35:40 +0500

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,24 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
 import os
+import chromedriver_autoinstaller
+from dotenv import load_dotenv
 
 load_dotenv()
 
 def before_all(context):
+    print("▶ Headless holat aniqlanmoqda...")
+    
+    # ChromeDriver'ni avtomatik yuklash
+    chromedriver_autoinstaller.install()
+
     chrome_options = Options()
 
     if os.getenv("HEADLESS", "false").lower() == "true":
-        print("▶ Headless rejimda ishga tushyapti...")
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--window-size=1920,1080')
+        print("▶ Headless rejimda ishga tushyapti...")
     else:
-        print("▶ Vizual brauzer rejimida ishga tushyapti...")
-        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument('--start-maximized')
+        print("▶ Real brauzer rejimda ishga tushyapti...")
 
     context.browser = webdriver.Chrome(options=chrome_options)
 
 def after_all(context):
-    context.browser.quit()
+    if hasattr(context, "browser"):
+        context.browser.quit()
